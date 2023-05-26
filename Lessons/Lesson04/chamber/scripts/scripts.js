548a6c50c8e458e754025a31bf80889822c1610c
sartentooth <sebastiancascallares@gmail.com>
2023-05-26 20:03:39 -0300

    update and fix some errors in small css file

--- Lessons/Lesson04/chamber/scripts/scripts.js
+++ Lessons/Lesson04/chamber/scripts/scripts.js
@@ -31,12 +31,14 @@ const year = date.getFullYear();
 document.querySelector("#year").innerHTML = year;
 document.querySelector("#current-date").innerHTML = document.lastModified;
 
-// banner 
+// banner that appears only two days a week 
 const day = date.getDay();
 const promo = document.querySelector('.banner');
 const promoText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 
-if (date.getDay() == 5 || date.getDay() == 2) {
+// by default only show on monday (1) and tuesday (2).
+// for testing porpuse change the value to another number between 0 to 6 (getDay()).
+if (date.getDay() == 1 || date.getDay() == 2) {
    
     promo.innerHTML = promoText;
     promo.style.display = 'block';
@@ -44,3 +46,6 @@ if (date.getDay() == 5 || date.getDay() == 2) {
 else {
     promo.style.display = 'none';
 }
+
+// I used this for reference about how to hide elements. 
+// https://norfipc.com/inf/javascript-como-ocultar-mostrar-elementos-paginas-web.html
