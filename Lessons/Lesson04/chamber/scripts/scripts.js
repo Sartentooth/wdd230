39109d48866f34e1a1012d9341d5bc9f45cf7346
sartentooth <sebastiancascallares@gmail.com>
2023-05-26 19:00:17 -0300

    lesson 5 ready

--- Lessons/Lesson04/chamber/scripts/scripts.js
+++ Lessons/Lesson04/chamber/scripts/scripts.js
@@ -3,7 +3,7 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
-    
+
 }
 
 const x = document.getElementById('hamburgerBtn');
@@ -18,16 +18,29 @@ document.querySelector(".current-dateTime").textContent = fulldate;
 const date = new Date();
 const year = date.getFullYear();
 // const options = {
-    // weekday: "long",
-    // day: "numeric",
-    // month: "long",
-    // year: "numeric",
-    // hour: "numeric",
-    // minute: "numeric",
-    // second: "numeric",
-    // hour12: false
+// weekday: "long",
+// day: "numeric",
+// month: "long",
+// year: "numeric",
+// hour: "numeric",
+// minute: "numeric",
+// second: "numeric",
+// hour12: false
 // };
 // const fullDate = date.toLocaleDateString('en-US', options);
 document.querySelector("#year").innerHTML = year;
 document.querySelector("#current-date").innerHTML = document.lastModified;
 
+// banner 
+const day = date.getDay();
+const promo = document.querySelector('.banner');
+const promoText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+
+if (date.getDay() == 5 || date.getDay() == 2) {
+   
+    promo.innerHTML = promoText;
+    promo.style.display = 'block';
+}
+else {
+    promo.style.display = 'none';
+}
