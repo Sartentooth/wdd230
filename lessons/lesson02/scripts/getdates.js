e32992a6ee4d3c478120287868ad2b4e9055c4a5
sartentooth <sebastiancascallares@gmail.com>
2023-09-23 15:20:34 -0300

    Design Principles ready

---
+++ lessons/lesson02/scripts/getdates.js
@@ -0,0 +1,8 @@
+const date = new Date();
+const year = date.getFullYear();
+
+// Last Modified 
+document.querySelector("#year").innerHTML = year;
+document.querySelector("#lastModified").innerHTML = document.lastModified;
+
+
