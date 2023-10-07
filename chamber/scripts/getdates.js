dbe1312fda18f782dcf9fd175692b1fdc05dba6a
sartentooth <sebastiancascallares@gmail.com>
2023-10-07 16:59:10 -0300

    code update

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -4,11 +4,11 @@ const year = date.getFullYear();
 // List of the name of the week, Index starts in 0 to 6.
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-// weather static for now.
-const current_day = weekday[date.getDay()];
-document.querySelector("#day-name").innerHTML = current_day;
-document.querySelector("#wind-info").innerHTML = "windy";
-document.querySelector("#temperature").innerHTML = "23°C";
+// Header Current Date
+const current = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(current);
+document.querySelector(".current-dateTime").textContent = fulldate;
+
 
 // Page Visitor static for now.
 document.querySelector("#page-visitor").innerHTML = "1";
