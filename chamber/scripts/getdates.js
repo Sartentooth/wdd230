d7ca1a81b43ef4b417cee1f06d0eab644c765128
sartentooth <sebastiancascallares@gmail.com>
2023-10-07 17:56:41 -0300

    update

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,20 +1,13 @@
-const date = new Date();
-const year = date.getFullYear();
-
 // List of the name of the week, Index starts in 0 to 6.
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 // Header Current Date
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date);
-document.querySelector(".current-dateTime").textContent = fulldate;
-
-
-// Page Visitor static for now.
-document.querySelector("#page-visitor").innerHTML = "1";
-
+const current = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(current);
+document.querySelector(".current-dateTime").innerHTML = fulldate;
 
 // Last Modified 
+const date = new Date();
+const year = date.getFullYear();
 document.querySelector("#year").innerHTML = year;
 document.querySelector("#lastModified").innerHTML = document.lastModified;
-
-
