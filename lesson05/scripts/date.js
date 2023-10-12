db72c7936328ae20b6e393bc007934826bdde307
sartentooth <sebastiancascallares@gmail.com>
2023-10-12 20:06:46 -0300

    bom ready

---
+++ lesson05/scripts/date.js
@@ -0,0 +1,5 @@
+const date = new Date();
+const year = date.getFullYear();
+
+document.querySelector("#year").innerHTML = year;
+document.querySelector("#current-date").innerHTML = document.lastModified;
