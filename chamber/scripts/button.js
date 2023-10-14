9607dd1497e021e29ac45c7b0593c50e21754ddc
sartentooth <sebastiancascallares@gmail.com>
2023-10-14 16:55:37 -0300

    update footer, main, border and shadows

--- chamber/scripts/button.js
+++ chamber/scripts/button.js
@@ -11,23 +11,25 @@ const body = document.querySelector("body");
 const header = document.querySelector("header");
 const nav = document.querySelector("nav");
 const main = document.querySelector("main");
+const hr = document.querySelector("hr");
 const footer = document.querySelector("footer");
 
 modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("🌑")) {
+	if (modeButton.textContent.includes("🌒")) {
 		body.style.background = "#282829";
-		body.style.border = "1px solid #79D509";
-		header.style.background = "#282829";
-		nav.style.background = "#282829";
-		main.style.background = "#282829";
-		footer.style.background = "#282829";
+		body.style.border = "none";
+		header.style.background = "#191919";
+		nav.style.background = "#424242";
+		main.style.background = "#191919";
+		footer.style.background = "#424242";
 		header.style.color = "#F5F5F5";
 		main.style.color = "#F5F5F5";
 		footer.style.color = "#F5F5F5";
-		modeButton.textContent = "🌕";
+		hr.style.backgroundImage = "-webkit-linear-gradient(left, #424242, #F5F5F5, #424242)";
+		modeButton.textContent = "🌘";
 	} else {
 		body.style.background = "#F5F5F5";
-		body.style.border = "1px solid #1D3559";
+		body.style.border = "none";
 		header.style.background = "#1D3559";
 		nav.style.background = "#08689B"
 		main.style.background = "#F5F5F5";
@@ -35,6 +37,8 @@ modeButton.addEventListener("click", () => {
 		header.style.color = "#F5F5F5";
 		main.style.color = "#000";
 		footer.style.color = "#F5F5F5";
-		modeButton.textContent = "🌑";
+		hr.style.backgroundImage = "-webkit-linear-gradient(left, #08689B, #F5F5F5, #08689B)";
+		modeButton.textContent = "🌒";
+
 	}
 });
