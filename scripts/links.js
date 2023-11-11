8e0b795778e3248f9f7c3a6ba7a9f9bff5b78f55
sartentooth <sebastiancascallares@gmail.com>
2023-11-11 16:56:14 -0300

    course home modify with JSON Dinamics menu

---
+++ scripts/links.js
@@ -0,0 +1,35 @@
+const baseURL = "https://sartentooth.github.io/wdd230/";
+const linksURL = "https://sartentooth.github.io/wdd230/data/links.json";
+const list = document.querySelector(".list");
+
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data.lessons);
+}
+
+const displayLinks = (weeks) => {
+
+  weeks.forEach((week) => {
+    let listItem = document.createElement("li");
+    listItem.textContent = `Lesson ${week.lesson}: `;
+    
+    week.link.forEach((link, linkIndex) => {
+      let linkElement = document.createElement("a");
+      linkElement.setAttribute("href", link.url);
+      
+      if (linkIndex < week.link.length -1){
+        linkElement.textContent = ` ${link.title} |`;
+      } else {
+        linkElement.textContent = ` ${link.title}`;
+      }
+      
+      listItem.append(linkElement);
+      
+    });
+    
+    list.append(listItem);
+  });
+}
+
+getLinks();
