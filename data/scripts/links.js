4e934fd9679a3ee7fa592280b13e22580d3a4a6d
sartentooth <sebastiancascallares@gmail.com>
2023-11-10 23:15:21 -0300

    update

--- data/scripts/links.js
+++ data/scripts/links.js
@@ -1,2 +1,11 @@
 const baseURL = "https://sartentooth.github.io/wdd230/";
 const linksURL = "https://sartentooth.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+    
+  }
+  
+  getLinks();
