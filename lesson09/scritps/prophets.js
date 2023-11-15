2bc4eb96660980d160371aa840a4c049e873fd6a
sartentooth <sebastiancascallares@gmail.com>
2023-11-15 18:52:58 -0300

    some changes for better understand

--- lesson09/scritps/prophets.js
+++ lesson09/scritps/prophets.js
@@ -23,23 +23,28 @@ const displayProphets = (prophets) => {
         let years = document.createElement("p");
         let portrait = document.createElement("img");
 
+        // check if the prophet is alive
+        const deathAlive = `${(prophet.death == null) ? "alive" : "died, " + prophet.death}`;
+        const orderOf = `${(prophet.death == null) ? "he is the " + prophet.order : "he was the " + prophet.order}° prophet of the Church`;
+        const servedYears = `${(prophet.death == null) ? "currently serving as a prophet for " + prophet.length : "and served as a prophet for " + prophet.length} years`;
+
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
         birthdate.textContent = `was born on ${prophet.birthdate}`;
         birthplace.textContent = `in the city of ${prophet.birthplace}`;
         numofchildren.textContent = `he had ${prophet.numofchildren} childrens`;
-        death.textContent = `${(prophet.death == null) ? "alive" : "died, " + prophet.death}`; // check if the prophet is alive
-        order.textContent = `${(prophet.death == null) ? "he is the " + prophet.order : "he was the " + prophet.order}° prophet of the Church`;
-        years.textContent = `and served as a prophet for ${prophet.length} years`;
+        death.textContent = deathAlive;
+        order.textContent = orderOf;
+        years.textContent = servedYears;
 
         portrait.setAttribute("src", prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
-    
+
         // Append the section(card) with the created elements
         card.append(fullName, birthdate, birthplace, numofchildren, death, order, years, portrait); //fill in the blank
-      
+
         cards.appendChild(card);
     });
 }
