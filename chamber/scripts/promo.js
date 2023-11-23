66b3deca400964a776245d1ef4830c7a650e852b
sartentooth <sebastiancascallares@gmail.com>
2023-11-22 22:55:44 -0300

    promo ready

---
+++ chamber/scripts/promo.js
@@ -0,0 +1,18 @@
+// banner that appears only two days a week 
+const day = date.getDay();
+const promo = document.querySelector('.banner');
+const promoText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+
+// by default only show on monday (1) and tuesday (2).
+// for testing porpuse change the value to another number between 0 to 6 (getDay()).
+if (date.getDay() == 6 || date.getDay() == 3) {
+   
+    promo.innerHTML = promoText;
+    promo.style.display = 'block';
+}
+else {
+    promo.style.display = 'none';
+}
+
+// I used this for reference about how to hide elements. 
+// https://norfipc.com/inf/javascript-como-ocultar-mostrar-elementos-paginas-web.html
