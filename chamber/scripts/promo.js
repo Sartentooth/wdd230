a87b1f7a9f8f46f045f75a43d3372369bb3b5bfe
sartentooth <sebastiancascallares@gmail.com>
2023-11-23 00:31:11 -0300

    ready

--- chamber/scripts/promo.js
+++ chamber/scripts/promo.js
@@ -2,17 +2,25 @@
 const day = date.getDay();
 const promo = document.querySelector('.banner');
 const promoText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-
-// by default only show on monday (1) and tuesday (2).
+const button = document.querySelector(".close");
+// by default only show on monday (1) tuesday (2) and wednesday (3).
 // for testing porpuse change the value to another number between 0 to 6 (getDay()).
-if (date.getDay() == 6 || date.getDay() == 3) {
-   
+if (date.getDay() >= 1 && date.getDay() <= 4) {
+
     promo.innerHTML = promoText;
     promo.style.display = 'block';
+
+    button.addEventListener("click", () => {
+      
+        if (button.textContent.includes("X")) {
+            promo.style.display = 'none';
+            button.style.display = 'none';
+        }
+    });
 }
 else {
     promo.style.display = 'none';
 }
 
-// I used this for reference about how to hide elements. 
+// I used this for reference about how to hide elements.
 // https://norfipc.com/inf/javascript-como-ocultar-mostrar-elementos-paginas-web.html
