a19084806ede8dbf98663d79674dbfc29b2d47c5
sartentooth <sebastiancascallares@gmail.com>
2023-11-23 00:38:59 -0300

    fixed js code promo button

--- chamber/scripts/promo.js
+++ chamber/scripts/promo.js
@@ -3,10 +3,11 @@ const day = date.getDay();
 const promo = document.querySelector('.banner');
 const promoText = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
 const button = document.querySelector(".close");
+
 // by default only show on monday (1) tuesday (2) and wednesday (3).
 // for testing porpuse change the value to another number between 0 to 6 (getDay()).
 if (date.getDay() >= 1 && date.getDay() <= 3) {
-
+    
     promo.innerHTML = promoText;
     promo.style.display = 'block';
 
@@ -20,6 +21,7 @@ if (date.getDay() >= 1 && date.getDay() <= 3) {
 }
 else {
     promo.style.display = 'none';
+    button.style.display = 'none';
 }
 
 // I used this for reference about how to hide elements.
